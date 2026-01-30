bf71e7341cc99b16c34ac90733ce828de53e9ea8
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-01-30 15:14:57 -0500

    fix: cambie el authguardad por el nuevo que cree

--- Frontend/src/app/pages/pages.routes.ts
+++ Frontend/src/app/pages/pages.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { StarterComponent } from './starter/starter.component';
-import { AuthGuard } from './authentication/auth.guard';
+import { authGuard } from '../guards/auth.guard';
 
 export const PagesRoutes: Routes = [
   {
     path: '',
     component: StarterComponent,
-    canActivate: [AuthGuard],
+    canActivate: [authGuard],
 
     data: {
       title: 'Starter',
